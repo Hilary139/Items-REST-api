7a770d62e4d6eb4d00fe80a75db4b3da83c452dc
hilary139 <aghasilihilary@gmail.com>
2023-02-08 17:08:02 +0100

    changed api

--- RESTapi/settings.py
+++ RESTapi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%c1=crxvy6y83+42u_1^hgv*%7%+c)-44h67v=@&+_azzpxuyk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
 
 # Application definition
--- base/views.py
+++ base/views.py
@@ -1,3 +1,110 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework import status
+from  base.models import Item
+from .serializers import ItemSerializer
+from rest_framework.parsers import JSONParser 
 
-# Create your views here.
+
+
+@api_view(['GET'])
+def apiOverview(request):
+    api_urls = {
+        'Get All Items' : '/getItems/',
+        'Single Item Detail' : '/itemDetail/<str:pk>/',
+        'Select, Update, Delete Single Item' : '/itemMakeChange/<str:pk>/',
+        'Create Item ' : '/addItem/',
+        'Update Item ' : '/updateItem/<str:pk>/',
+        'Delete Item ' : '/deleteItem/<str:pk>/',
+    }
+    return Response(api_urls, status.HTTP_200_OK)
+
+
+
+#* Get all Items
+'''
+@api_view(['GET'])
+def getItems(request):
+    if request.method == 'GET':
+        try:    
+            items = Item.objects.all()
+        except:
+            return Response({
+            "error": "Items Not Found"
+            }, status.HTTP_404_NOT_FOUND)
+
+        serializer = ItemSerializer(items, many = True)
+        return Response(serializer.data, status.HTTP_200_OK)
+    else:
+        return Response(serializer.error, status.HTTP_400_BAD_REQUEST)
+'''
+
+#* Create Items
+@api_view(['GET','POST'])
+def getItems(request, format=None):
+
+    if request.method == 'GET':
+        items = Item.objects.all()
+        serializer = ItemSerializer(items, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    elif request.method == 'POST':
+        item_data = JSONParser().parse(request)
+        serializer = ItemSerializer(data=item_data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)  
+
+
+#* Get Item Detail
+@api_view(['GET'])
+def itemDetail(request, pk):
+    if request.method == 'GET':
+        try:
+            items = Item.objects.get(id=pk)
+        except:
+            return Response({
+                "error": "Invalid book id"
+            }, status.HTTP_404_NOT_FOUND)    
+
+        serializer = ItemSerializer(items, many = False)
+        return Response(serializer.data, status.HTTP_200_OK)
+    else:
+        return Response(serializer.error, status.HTTP_400_BAD_REQUEST)
+
+
+
+#* Update Item
+@api_view(['POST'])
+def updateItem(request, pk): 
+    if request.method == 'POST':
+        try:
+            item = Item.objects.get(id=pk)
+        except:
+            return Response({
+                "error": "Unable to update item"
+            },status.HTTP_400_BAD_REQUEST)  
+
+    serializer = ItemSerializer(instance=item,  data = request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status.HTTP_202_ACCEPTED)    
+    return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+
+#* Delete Item
+@api_view(['DELETE'])
+def deleteItem(request, pk):
+    if request.method == 'DELETE':
+        try:
+            item = Item.objects.get(id=pk)
+        except:
+            return Response({
+                "error": "Unable to delete item"
+            }, status.HTTP_400_BAD_REQUEST)    
+    item.delete()
+    return Response("Item deleted successfully!!", status.HTTP_200_OK)    
+
+
+
